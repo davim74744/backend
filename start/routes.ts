c7d32c52890fe103eb2dc36ce6ddc89e10e434e9
davim74744 <davim74744@gmail.com>
2025-08-31 20:24:31 -0300

    Sua mensagem descrevendo as mudanças

--- start/routes.ts
+++ start/routes.ts
@@ -1,7 +1,12 @@
-// start/routes.ts
-import PixController from '#controllers/pix_controller'
 import router from '@adonisjs/core/services/router'
+
+
+import PixController from '#controllers/pix_controller'
+import CartaosController from '#controllers/cartaos_controller'
+
 
 
 // Adicione esta linha:
 router.post('/api/pix/generate', [PixController, 'generatePixPayment'])
+router.post('/api/gerar/cartao', [CartaosController,'processCreditPayment'])
+router.post('/api/processar/pagamento', [CartaosController,'processCreditPayment'])
